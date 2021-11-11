0d181a7ee41cc92bef76c051170d3d193516ca46
AkiraToya <akira_toya2006@yahoo.com>
2021-11-11 14:15:14 +0700

    Styling React Pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPaginate from 'react-paginate';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPaginate pageCount={10}
+        containerClassName="pagination-container"
+        pageClassName="page"
+        previousClassName="prev"
+        nextClassName="next"
+        breakClassName="break"
+        activeClassName="active"
+      />
     </div>
   );
 }
