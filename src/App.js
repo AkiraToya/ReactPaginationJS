5627b5bf4422fd92c16e3c0c7ef48cd096711473
AkiraToya <akira_toya2006@yahoo.com>
2021-11-11 14:20:49 +0700

    JS Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 import ReactPaginate from 'react-paginate';
+import { useEffect } from 'react';
 
 function App() {
+
+  let fetchUsers = async () => {
+    let request = await fetch("https://jsonplaceholder.typicode.com/users?_page=1&_limit=3")
+    let requestedData = await request.json()
+
+    let totalPages = Math.ceil(10/3)
+    let datas = requestedData
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   return (
     <div className="App">
       <ReactPaginate pageCount={10}
